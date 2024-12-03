faed1525f54e814a09e216d5056221dbe44d6d72
Jonissa175 <jp.pters@me.com>
2024-12-03 09:58:53 +0100

    Start of exercises

---
+++ main.js
@@ -0,0 +1,60 @@
+
+console.log("Starting javascript");
+
+//Ex 1
+const myName = "Jonas";
+console.log(myName);
+
+//Ex 2
+const myAge = 18;
+console.log(myAge);
+
+//Ex 3
+const juliaAge = 32;
+const ageDiff = myAge - juliaAge;
+console.log(ageDiff);
+
+//Ex 4/5
+if (myAge < 21) {
+  console.log("You are not older than 21");
+} else {
+  console.log("You are older than 21");
+}
+
+if (myAge < juliaAge) {
+  console.log("Julia is older than you");
+}
+if (myAge > juliaAge) {
+  console.log("Julia is younger than you");
+}
+if (myAge == juliaAge) {
+  console.log("You have the same age as Julia");
+}
+
+//Ex 6
+const myClass = ["Hasan", "Dima", "Thomas", "Maria", "Diogo", "Raul"]
+    console.log(myClass[0])
+    console.log(myClass[5])
+
+
+for (let i = 0; i < myClass.length; i++) {
+    console.log(myClass[i])
+    
+}
+
+//Ex 7
+
+
+const evenAges = [29, 32, 33, 30, 30, 40]
+let i = 0;
+while (i < evenAges.length) {
+    console.log(evenAges[i])
+    i++
+}
+
+//Ex 8
+
+//Ex 9
+
+//Ex 10
+
